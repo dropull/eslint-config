59069981d9471e70e594d24c16897c6367ef2f67
Lucas Cândido de Souza Silva <lcdss@live.com>
2022-12-14 19:26:59 -0300

    chore: bump dependencies

--- index.js
+++ index.js
@@ -9,13 +9,10 @@ module.exports = {
     'prettier',
   ],
   rules: {
+    'import/extensions': ['error', 'never'],
     'import/prefer-default-export': 'off',
     'promise/always-return': 'off',
     'promise/catch-or-return': ['error', { allowFinally: true }],
-    'unicorn/filename-case': [
-      'error',
-      { cases: { camelCase: true, pascalCase: true } },
-    ],
     'unicorn/no-array-for-each': 'off',
     'unicorn/no-array-reduce': 'off',
     'unicorn/no-null': 'off',
