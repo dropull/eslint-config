32441b25efbcdb5adff0cd47304364e25d86d1a5
Lucas Cândido de Souza Silva <lcdss@live.com>
2023-06-18 22:28:45 -0300

    feat: enable import recommended settings

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 module.exports = {
   ignorePatterns: ['**/node_modules'],
   reportUnusedDisableDirectives: true,
-  plugins: ['import'],
   extends: [
     'plugin:eslint-comments/recommended',
     'plugin:promise/recommended',
+    'plugin:import/recommended',
     'plugin:unicorn/recommended',
     'prettier',
   ],
   rules: {
     'import/extensions': ['error', 'never'],
+    'import/newline-after-import': 'error',
+    'import/no-deprecated': 'warning',
+    'import/no-self-import': 'error',
+    'import/no-unused-modules': [
+      'error',
+      { missingExports: true, unusedExports: true },
+    ],
     'import/prefer-default-export': 'off',
     'promise/always-return': 'off',
     'promise/catch-or-return': ['error', { allowFinally: true }],
